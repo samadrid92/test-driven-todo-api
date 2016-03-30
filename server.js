b84d06880a5b32c23f0b0e194afbea6ad03e0fd0
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-29 21:21:53 -0700

    got up to update

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -52,18 +52,39 @@ app.get('/api/todos/search', function search(req, res) {
 app.get('/api/todos', function index(req, res) {
   /* This endpoint responds with all of the todos
    */
+   res.send({todos: todos});
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   //old code
+  //  var id = req.body._id;
+  //  var task = req.body.task;
+  //  var desc = req.body.description;
+  //  var newTask = {task: task, description: desc, id: id} ;
+  //  todos.push({newTask});
+  //  res.send(req.body);
+
+  var newTodo = req.body;
+  if (todos.length > 0) {
+    newTodo._id = todos[todos.length - 1]._id + 1;
+  } else {
+    newTodo._id = 1;
+  }
+  todos.push(newTodo);
+  res.json(newTodo);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
+   var id = req.params.id;
+
+   res.json(todos[id-1]);
+
 });
 
 app.put('/api/todos/:id', function update(req, res) {
@@ -78,6 +99,12 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with success.
    */
+   var todoId = parseInt(req.params.id);
+   var todoToDelete = todos.filter(function (todo) {
+    return todo._id == todoId;
+  })[0];
+  todos.splice(todos.indexOf(todoToDelete), 1);
+  res.json(todoToDelete);
 });
 
 /**********
